97f60f1a38366c71e43a29ad413e2ff2bd94a2bf
abhiram <abhiramashok1062004@gmail.com>
2025-07-03 00:35:51 +0530

    Initial Commit

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -10,6 +10,7 @@ import LinearGradient from "react-native-linear-gradient";
 import SaveIcon from '../../assets/svg/SaveIcon.svg'
 import MagicPencilIcon from '../../assets/svg/MagicPencilIcon.svg';
 import Pattern from '../../assets/svg/Pattern1.svg';
+import PlusIcon from '../../assets/svg/PlusIcon.svg';
 import {PieChart} from "react-native-chart-kit";
 
 const App = () => {
@@ -94,55 +95,59 @@ const App = () => {
             <LinearGradient
                 colors={['#394f45', '#282828', '#282828']}
                 locations={[0, 0.2, 1]}
-                className="flex-1 flex-col gap-5"
+                className="flex-1 flex-col gap-5 pt-8"
             >
-                <ScrollView className="flex-1 px-6 pt-8">
-                    {/* Header Section */}
-                    <View className="mb-8 flex flex-row justify-center items-start">
-                        <View className="flex-1">
-                            <Text className="text-white text-2xl font-interSemiBold mb-4">
-                                Your Net{'\n'}Worth
+
+                {/* Header Section */}
+                <View className="mt-2 flex flex-row justify-center items-start px-6">
+                    <View className="flex-1">
+                        <Text className="text-white text-2xl font-interSemiBold mb-4">
+                            Your Net{'\n'}Worth
+                        </Text>
+                        <Text className="text-primary text-4xl font-interSemiBold mb-6">
+                            $1,000,000
+                        </Text>
+                    </View>
+
+                    {/* Growth Card */}
+                    <View
+                        className="relative bg-[#3b7e5e] border-[1.5px] border-[#46aa7c] rounded-2xl p-4 flex-row items-center justify-between">
+                        <View className="">
+                            <Text className="text-white text-sm font-interMedium mb-1">
+                                + $124,900.85
                             </Text>
-                            <Text className="text-primary text-4xl font-interSemiBold mb-6">
-                                $1,000,000
+                            <Text className="text-white text-2xl font-interSemiBold mb-1">
+                                10%
+                            </Text>
+                            <Text className="text-white text-sm font-interMedium">
+                                Growth this month
                             </Text>
                         </View>
-
-                        {/* Growth Card */}
-                        <View
-                            className="relative bg-[#3b7e5e] border-[1.5px] border-[#46aa7c] rounded-2xl p-4 flex-row items-center justify-between">
-                            <View className="">
-                                <Text className="text-white text-sm font-interMedium mb-1">
-                                    + $124,900.85
-                                </Text>
-                                <Text className="text-white text-2xl font-interSemiBold mb-1">
-                                    10%
-                                </Text>
-                                <Text className="text-white text-sm font-interMedium">
-                                    Growth this month
-                                </Text>
-                            </View>
-                            <View className="absolute left-0 top-0">
-                                <Pattern/>
-                            </View>
+                        <View className="absolute left-0 top-0">
+                            <Pattern/>
                         </View>
                     </View>
+                </View>
+
+
+                <View className="flex-row items-center justify-between px-6 mb-2">
+                    <View className="flex-row items-center">
+                        <Text className="text-white text-lg font-interSemiBold mr-2">
+                            Allocations
+                        </Text>
+                        <MagicPencilIcon/>
+                    </View>
+                    <TouchableOpacity>
+                        <Text className="text-primary text-md font-interMedium">
+                            + Add New
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+
+                <ScrollView className="flex-1 px-6">
 
                     {/* Allocations Section */}
                     <View className="mb-6">
-                        <View className="flex-row items-center justify-between mb-6">
-                            <View className="flex-row items-center">
-                                <Text className="text-white text-lg font-interSemiBold mr-2">
-                                    Allocations
-                                </Text>
-                                <MagicPencilIcon/>
-                            </View>
-                            <TouchableOpacity>
-                                <Text className="text-primary text-md font-interMedium">
-                                    + Add New
-                                </Text>
-                            </TouchableOpacity>
-                        </View>
 
                         {/* Allocation Cards */}
                         {allocations.map((allocation) => (
@@ -150,19 +155,21 @@ const App = () => {
                                 key={allocation.id}
                                 className=" bg-[#494949] rounded-xl p-4 mb-4 flex flex-row items-start border-[1.5px] border-[#666666]"
                             >
-                                <PieChart
-                                    data={pieData}
-                                    width={65}
-                                    height={65}
-                                    chartConfig={{
-                                        color: (opacity = 1) => `rgba(255, 0, 25, ${opacity})`,
-                                    }}
-                                    accessor="population"
-                                    backgroundColor="transparent"
-                                    paddingLeft="0"
-                                    hasLegend={false}
-                                    absolute
-                                />
+                                <View className="w-[60px] h-[60px] overflow-hidden mr-3 flex items-center justify-center rounded-full">
+                                        <PieChart
+                                            data={pieData}
+                                            width={65}
+                                            height={65}
+                                            chartConfig={{
+                                                color: (opacity = 1) => `rgba(255, 0, 25, ${opacity})`,
+                                            }}
+                                            accessor="population"
+                                            backgroundColor="transparent"
+                                            paddingLeft="16"
+                                            hasLegend={false}
+                                            absolute
+                                        />
+                                </View>
 
                                 <View className="flex-1">
                                     <Text className="text-white text-base font-interMedium mb-1">
@@ -189,11 +196,9 @@ const App = () => {
 
                         {/* Add New Allocation Button */}
                         <TouchableOpacity
-                            className="border-2 border-dashed border-gray-600 rounded-2xl p-6 items-center justify-center mt-4 mb-40">
-                            <View className="flex-row items-center">
-                                <View className="w-6 h-6 bg-green-500 rounded-full items-center justify-center mr-2">
-                                    <Text className="text-white text-sm font-interSemiBold">+</Text>
-                                </View>
+                            className="bg-[#363d3a] border-dashed border-[1.5px] border-[#666666] rounded-2xl px-4 py-8 items-center justify-center mt-4 mb-40">
+                            <View className="flex flex-row items-center justify-center gap-2">
+                                <PlusIcon/>
                                 <Text className="text-primary text-base font-interSemiBold">
                                     Add New Allocation
                                 </Text>
