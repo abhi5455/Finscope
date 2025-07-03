6c994ac168570a15690726da8b858d2fa07489aa
abhiram <abhiramashok1062004@gmail.com>
2025-07-03 21:29:28 +0530

    Home Page

--- src/navigation/TabNavigator/index.tsx
+++ src/navigation/TabNavigator/index.tsx
@@ -1,5 +1,5 @@
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
-import {Dimensions, StyleSheet, Text, View} from "react-native";
+import {Dimensions, StyleSheet, View} from "react-native";
 import AnimIcon from "../../common/AnimIcon.tsx";
 import HomeScreen from "../../screens/HomeScreen";
 import HomeIcon from '../../assets/svg/HomeIcon.svg'
